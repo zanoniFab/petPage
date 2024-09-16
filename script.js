dabab78b6f62357371a3c484a77f8c1a1e2d97f6
zanoniFab <fabianezanoni@gmail.com>
2024-09-16 00:12:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var racoesSecas = [
+    {
+        nome: "Birbo Carne",
+        imagem: "./assets/racoes/carne",
+        valor: 28,
+        descricao: "Com 8% de proteína, sabor carne."
+    },
+    {
+        nome: "Birbo Frango",
+        imagem: "./assets/racoes/frango",
+        valor: 30,
+        descricao: "Com 10% de proteína, sabor frango."
+    },
+    {
+        nome: "Birbo Legumes",
+        imagem: "./assets/racoes/legumes",
+        valor: 25,
+        descricao: "Com 20% de proteína, sabor legumes."
+    }
+
+]
+
+var racoesMolhadas = [
+    {
+        nome: "Birbo Carne",
+        imagem: "./assets/racoes/carneMol",
+        valor: 28,
+        descricao: "Com 8% de proteína, sabor carne."
+    },
+    {
+        nome: "Birbo Frango",
+        imagem: "./assets/racoes/frangoMol",
+        valor: 30,
+        descricao: "Com 10% de proteína, sabor frango."
+    },
+    {
+        nome: "Birbo Legumes",
+        imagem: "./assets/racoes/legumesMol",
+        valor: 25,
+        descricao: "Com 20% de proteína, sabor legumes."
+    }
+
+]
+
+function listarProdutos() {
+    let elemento = document.querySelector("#listaRacoes");
+    racoesMolhadas.forEach(racao => {
+        `
+        `
+    });
+}
